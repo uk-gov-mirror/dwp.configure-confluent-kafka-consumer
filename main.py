e0e4e34d1a78889b62672d5bdbab13cb6d60cac7
dou9g <39494020+dou9g@users.noreply.github.com>
2019-03-18 15:52:28 +0000

    introduced configuration insertion for worker connectors (#5)
    * introduced configuration insertion for worker connectors & cleardown existing configs before reinserting
    
    * amendments from comments completed from last pull request 5
    
    * amendments from comments completed from last pull request
    
    * amendments to connector name and modified PUT command to include config
    
    * amendments to connector name and modified PUT command to include config
    and removed if statement and outdent logger.debug
    
    * amendments to connector name and modified PUT command to include config
    and removed if statement and outdent logger.debug
    
    * requests amended as per
    
    * requests amended as per including new reference for existing_connector built from for in
    
    * reusing config for put n post
    
    * PR comments amended

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-import sys
 import argparse
 import boto3
+import json
 import logging
 import os
-import json
 import requests
+import sys
 
 # Initialise logging
 logger = logging.getLogger(__name__)
@@ -21,18 +21,34 @@ def get_parameters():
         description="Configure Confluent Kafka Consumers using RESTful API")
 
     # Parse command line inputs and set defaults
+    parser.add_argument('--connector-name', default='s3-sink')
     parser.add_argument('--aws-profile', default='default')
     parser.add_argument('--aws-region', default='eu-west-2')
+    parser.add_argument('--tasks-max', default='1')
+    parser.add_argument('--topics', default='')
+    parser.add_argument('--flush-size', default='1')
+    parser.add_argument('--port', default='8083')
+    parser.add_argument('--s3-bucket-name', default='')
 
     _args = parser.parse_args()
 
     # Override arguments with environment variables where set
+    if 'CONNECTOR_NAME' in os.environ:
+        _args.connector_name = os.environ['CONNECTOR_NAME']
     if 'AWS_PROFILE' in os.environ:
         _args.aws_profile = os.environ['AWS_PROFILE']
-
     if 'AWS_REGION' in os.environ:
         _args.aws_region = os.environ['AWS_REGION']
-
+    if 'TASKS_MAX' in os.environ:
+        _args.tasks_max = os.environ['TASKS_MAX']
+    if 'TOPICS' in os.environ:
+        _args.topics = os.environ['TOPICS']
+    if 'FLUSH_SIZE' in os.environ:
+        _args.flush_size = os.environ['FLUSH_SIZE']
+    if 'PORT' in os.environ:
+        _args.port = os.environ['PORT']
+    if 'S3_BUCKET_NAME' in os.environ:
+        _args.s3_bucket_name = os.environ['S3_BUCKET_NAME']
     return _args
 
 
@@ -52,12 +68,71 @@ def configure_confluent_kafka_consumer(event, args):
     if logger.isEnabledFor(logging.DEBUG):
         # Log everything from boto3
         boto3.set_stream_logger()
-        logger.debug(f"Using boto3 {boto3.__version__}")
+    logger.debug(f"Using boto3 {boto3.__version__}")
 
     logger.debug(event)
 
     message = json.loads(event['Records'][0]['Sns']['Message'])
     logger.debug(message)
+
+    private_ip = message['detail']['containers'][0]['networkInterfaces'][0]['privateIpv4Address']
+    logger.debug(private_ip)
+
+    connector_config = {
+        "connector.class": "io.confluent.connect.s3.S3SinkConnector",
+        "tasks.max": args.tasks_max,
+        "topics": args.topics,
+        "flush.size": args.flush_size,
+        "s3.region": args.aws_region,
+        "s3.bucket.name": args.s3_bucket_name,
+        "storage.class": "io.confluent.connect.s3.storage.S3Storage",
+        "format.class": "io.confluent.connect.s3.format.avro.AvroFormat",
+        "schema.generator.class": "io.confluent.connect.storage.hive.schema.DefaultSchemaGenerator",
+        "partitioner.class": "io.confluent.connect.storage.partitioner.DefaultPartitioner",
+    }
+
+    # Get a list of existing connectors
+
+    response = requests.get(f"http://{private_ip}:{args.port}/connectors")
+    existing_connectors = response.content.decode("utf-8")
+    logger.debug(existing_connectors)
+
+    # Check if required connector already exists
+
+    if args.connector_name in existing_connectors:
+        logger.debug("update connector [PUT]")
+        # PUT payload to update existing connector
+        payload = {
+            "config": connector_config
+        }
+
+        response = requests.put(f"http://{private_ip}:{args.port}/connectors/{args.connector_name}/config",
+                                json=payload)
+        logger.debug(response.content.decode("utf-8"))
+
+    else:
+
+        payload = {
+            "name": args.connector_name,
+            "config": connector_config
+        }
+
+    # POST payload if connectors don't exist
+
+        logger.debug("create connector [POST]")
+
+        response = requests.post(f"http://{private_ip}:{args.port}/connectors", json=payload)
+        logger.debug(response.content.decode("utf-8"))
+
+    # DELETE all others
+
+    for existing_connector in existing_connectors:
+        if existing_connector is not args.connector_name:
+
+            logger.debug("delete connector [DELETE]")
+
+            response = requests.delete(f"http://{private_ip}:{args.port}/connectors/{existing_connector}")
+            logger.debug(response.content.decode("utf-8"))
 
 
 if __name__ == "__main__":
